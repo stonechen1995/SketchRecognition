f07956be32530ae2c691f211991d236f4c3546d5
Lucas <lucas.lxl@bitme.fun>
2022-09-14 23:49:54 -0500

    refactor: rename variables and add space line

--- part1.py
+++ part1.py
@@ -3,22 +3,25 @@ import math
 import os
 from string import ascii_lowercase
 
+
 def delta(col, ind1, ind2=None):
-    if ind2 == None:
-        return col[ind1] - col[ind1-1]
+    if ind2 is None:
+        return col[ind1] - col[ind1 - 1]
     return col[ind1] - col[ind2]
 
-def cleanData(df):
+
+def clean_data(df):
     # delete rows that have repeated values that are the same as the one right above. 
-    try: 
+    try:
         for index, row in df.iterrows():
-            if row["x"] == df.iloc[index-1]["x"] and row["y"] == df.iloc[index-1]["y"]:
+            if row["x"] == df.iloc[index - 1]["x"] and row["y"] == df.iloc[index - 1]["y"]:
                 df = df.drop(labels=index, axis=0)
     except:
         pass
 
-def calculateRubine(df):
-    cleanData(df)
+
+def calculate_rubine(df):
+    clean_data(df)
 
     x_col = df["x"]
     y_col = df["y"]
@@ -28,64 +31,67 @@ def calculateRubine(df):
     y_max = max(y_col)
     y_min = min(y_col)
 
-    f1 = (x_col[2] - x_col[0]) / math.sqrt((y_col[2] - y_col[0])**2 + (x_col[2] - x_col[0])**2)
-    f2 = (y_col[2] - y_col[0]) / math.sqrt((y_col[2] - y_col[0])**2 + (x_col[2] - x_col[0])**2)
-    f3 = math.sqrt((y_max - y_min)**2 + (x_max - x_min)**2)
+    f1 = (x_col[2] - x_col[0]) / math.sqrt((y_col[2] - y_col[0]) ** 2 + (x_col[2] - x_col[0]) ** 2)
+    f2 = (y_col[2] - y_col[0]) / math.sqrt((y_col[2] - y_col[0]) ** 2 + (x_col[2] - x_col[0]) ** 2)
+    f3 = math.sqrt((y_max - y_min) ** 2 + (x_max - x_min) ** 2)
     f4 = math.atan2((y_max - y_min), (x_max - x_min))
-    f5 = math.sqrt((x_col[df.shape[0]-1] - x_col[0])**2 + (y_col[df.shape[0]-1] - y_col[0])**2)
-    f6 = (x_col[df.shape[0]-1] - x_col[0]) / f5
-    f7 = (y_col[df.shape[0]-1] - y_col[0]) / f5
+    f5 = math.sqrt((x_col[df.shape[0] - 1] - x_col[0]) ** 2 + (y_col[df.shape[0] - 1] - y_col[0]) ** 2)
+    f6 = (x_col[df.shape[0] - 1] - x_col[0]) / f5
+    f7 = (y_col[df.shape[0] - 1] - y_col[0]) / f5
 
     f8 = 0
     for i in range(1, df.shape[0], 1):
-        f8 += math.sqrt(delta(x_col, i)**2 + delta(y_col, i)**2)
+        f8 += math.sqrt(delta(x_col, i) ** 2 + delta(y_col, i) ** 2)
 
     f9 = 0
     f10 = 0
     f11 = 0
-    for i in range (2, df.shape[0], 1):
-        nominator = (-delta(y_col, i) * delta(x_col, i-1) + delta(y_col, i-1) * delta(x_col, i))
-        denominator = (delta(x_col, i) * delta(x_col, i-1) + delta(y_col, i)   * delta(y_col, i-1))
-        temp = math.atan2(nominator, denominator) 
+    for i in range(2, df.shape[0], 1):
+        nominator = (-delta(y_col, i) * delta(x_col, i - 1) + delta(y_col, i - 1) * delta(x_col, i))
+        denominator = (delta(x_col, i) * delta(x_col, i - 1) + delta(y_col, i) * delta(y_col, i - 1))
+        temp = math.atan2(nominator, denominator)
         f9 += temp
         f10 += abs(temp)
-        f11 += temp**2
+        f11 += temp ** 2
 
     f12 = 0
     for i in range(1, df.shape[0], 1):
         if delta(t_col, i) == 0: continue
-        f12 = max(f12, (delta(x_col, i)**2 + delta(y_col, i)**2) / delta(t_col, i)**2)
+        f12 = max(f12, (delta(x_col, i) ** 2 + delta(y_col, i) ** 2) / delta(t_col, i) ** 2)
 
-    f13 = t_col[df.shape[0]-1] - t_col[0]
+    f13 = t_col[df.shape[0] - 1] - t_col[0]
     result = [f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13]
     return result
 
-def generate1(): #run letter data
+
+def generate1():  # run letter data
     results = []
     col = ["sketch", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "f13"]
     for character in ascii_lowercase:
         for index in range(20):
-            path = "data/letters-csv/" + character + "/" + character + "_" + str(index+1) + ".csv"
+            path = "data/letters-csv/" + character + "/" + character + "_" + str(index + 1) + ".csv"
             df = pd.read_csv(path)
             # print(path)
-            result = calculateRubine(df)
-            result.insert(0, character + "_" + str(index+1))
+            result = calculate_rubine(df)
+            result.insert(0, character + "_" + str(index + 1))
             results.append(result)
     df_result = pd.DataFrame(results, columns=col)
     df_result.to_csv("features.csv")
 
-def generate2(): #run sample data to test the correctness of code
+
+def generate2():  # run sample data to test the correctness of code
     results = []
     col = ["sketch", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "f13"]
     for index in range(8):
-        path = "data/sample-csv/shape_" + str(index+1) + ".csv"
+        path = "data/sample-csv/shape_" + str(index + 1) + ".csv"
         df = pd.read_csv(path)
         # print(path)
-        result = calculateRubine(df)
-        result.insert(0, "shape_" + str(index+1))
+        result = calculate_rubine(df)
+        result.insert(0, "shape_" + str(index + 1))
         results.append(result)
     df_result = pd.DataFrame(results, columns=col)
     df_result.to_csv("features.csv")
+
 
 if __name__ == "__main__":
     generate1()
--- part2.py
+++ part2.py
@@ -4,38 +4,43 @@ import os
 import glob
 import matplotlib.pyplot as plt
 
+
 def delta(col, ind1, ind2=None):
-    if ind2 == None:
-        return col[ind1] - col[ind1-1]
+    if ind2 is None:
+        return col[ind1] - col[ind1 - 1]
     return col[ind1] - col[ind2]
 
-def cleanData(df):
+
+def clean_data(df):
     # delete rows that have repeated values that are the same as the one right above. 
-    try: 
+    try:
         for index, row in df.iterrows():
-            if row[1] == df.iloc[index-1][1] and row[2] == df.iloc[index-1][2]:
+            if row[1] == df.iloc[index - 1][1] and row[2] == df.iloc[index - 1][2]:
                 df = df.drop(labels=index, axis=0)
     except:
         pass
 
-def calculateRubine(df):
-    cleanData(df)
+
+def calculate_rubine(df):
+    clean_data(df)
     x_col = df.iloc[:, 1]
     y_col = df.iloc[:, 2]
     f8 = 0
     for i in range(1, df.shape[0], 1):
-        f8 += math.sqrt(delta(x_col, i)**2 + delta(y_col, i)**2)
+        f8 += math.sqrt(delta(x_col, i) ** 2 + delta(y_col, i) ** 2)
     return x_col, y_col, f8
 
-def indexPoint(df, startX, startY):
-    df[startX]
+
+# def indexPoint(df, startX, startY):
+#     df[startX]
+
 
 def resample():
     path = "resample-data/"
     csv_files = glob.glob(os.path.join(path, "*.csv"))
     for f in csv_files:
         df = pd.read_csv(f, header=None)
-        x_col, y_col, length = calculateRubine(df)
+        x_col, y_col, length = calculate_rubine(df)
         results = [[x_col[0], y_col[0]]]
         unit = length / 63
         ind = 1
@@ -44,14 +49,17 @@ def resample():
             # otherwise, index to next point and repeat the first step.
             f8 = 0
             num = 0
+            f8_change = 0
             while True:
                 if unit * ind > f8:
                     f8_change = math.sqrt(delta(x_col, num + 1) ** 2 + delta(y_col, num + 1) ** 2)
                     f8 += f8_change
                     num += 1
                 else:
-                    x_value = x_col[num - 1] + (unit * ind - (f8 - f8_change)) * (x_col[num] - x_col[num - 1]) / f8_change
-                    y_value = y_col[num - 1] + (unit * ind - (f8 - f8_change)) * (y_col[num] - y_col[num - 1]) / f8_change
+                    x_value = x_col[num - 1] + (unit * ind - (f8 - f8_change)) * (
+                            x_col[num] - x_col[num - 1]) / f8_change
+                    y_value = y_col[num - 1] + (unit * ind - (f8 - f8_change)) * (
+                            y_col[num] - y_col[num - 1]) / f8_change
                     results.append([x_value, y_value])
                     ind += 1
                     break
@@ -64,6 +72,6 @@ def resample():
         # fig, bx = plt.subplots()
         # bx.plot(df_result.iloc[:,0], df_result.iloc[:,1])
 
-        
+
 if __name__ == "__main__":
     resample()
